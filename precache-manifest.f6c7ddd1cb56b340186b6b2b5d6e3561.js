3494ffc5573324b3751032564954900a8e0359bc
GeorgPandeh <georgpandeh@gmail.com>
2019-07-24 14:46:17 +0300

    Updates

---
+++ precache-manifest.f6c7ddd1cb56b340186b6b2b5d6e3561.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416c6785a5b603b5eb53d4d9910aa54b",
+    "url": "/Writers-of-Belarus/index.html"
+  },
+  {
+    "revision": "da4f61bec62566456c51",
+    "url": "/Writers-of-Belarus/static/css/main.78ca6917.chunk.css"
+  },
+  {
+    "revision": "ef33df020e33e4ba7fe8",
+    "url": "/Writers-of-Belarus/static/js/2.cdd56b36.chunk.js"
+  },
+  {
+    "revision": "da4f61bec62566456c51",
+    "url": "/Writers-of-Belarus/static/js/main.ae5a86f4.chunk.js"
+  },
+  {
+    "revision": "dccc0355f9527061a3c5",
+    "url": "/Writers-of-Belarus/static/js/runtime~main.70f22e49.js"
+  }
+]);
