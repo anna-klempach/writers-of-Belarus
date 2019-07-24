37a6d117e93279a7f476b6a2a64c99884405e1e8
GeorgPandeh <georgpandeh@gmail.com>
2019-07-24 14:41:03 +0300

    init: start CodeJam-Culture-Portal

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Main from './components/main/Main';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Switch>
+        <Route exact path='/' component={Main}/>
+      </Switch>
+      </div>
+    )
+  }
+}
+
+export default App;
